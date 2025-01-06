96d6e9ec07badc59c7a54e230034dc8be23f53dd
Chris-Winnard <74295248+Chris-Winnard@users.noreply.github.com>
2025-01-06 19:41:49 +0000

    Completed third and final array task

--- src/Ch01-Arrays/01_07/begin/find-item.cpp
+++ src/Ch01-Arrays/01_07/begin/find-item.cpp
@@ -5,10 +5,28 @@ using namespace std;
 template <typename T>
 int findItem(const T arr[], size_t size, T target)
 {
-    // TODO: implement
+    int index = -1;
+    for (size_t i = 0; i < size; i++)
+    {
+        if (arr[i] == target)
+            index = i;
+    }
+    return index;
 }
 
 int main()
 {
+    int testArray[]{1, 3, 5};
+    int target = 5;
+
+    size_t size = sizeof(testArray) / sizeof(testArray[0]);
+    cout << "testArray first contains the target " << target << " at index: " << findItem(testArray, size, target) << endl;
+
+    std::string testArray2[]{"apple", "banana", "cherry"};
+    std::string target2 = "cake";
+
+    size_t size2 = sizeof(testArray2) / sizeof(testArray2[0]);
+    cout << "testArray first contains the target " << target2 << " at index: " << findItem(testArray2, size2, target2) << endl;
+
     return 0;
 }
